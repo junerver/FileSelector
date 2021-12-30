69e19ac4d1a2e1c5d3b33b66b8cc1a8cc24d6350
Junerver <junerver@qq.com>
2021-12-30 12:58:02 +0800

    backup

--- app/src/main/java/xyz/junerver/fsbrowser/MainActivity.kt
+++ app/src/main/java/xyz/junerver/fsbrowser/MainActivity.kt
@@ -25,17 +25,23 @@ class MainActivity : FileSelectorActivity() {
 
     override fun initAdapter() {
         super.initAdapter()
-        mFileAdapter.setListener { path ->
-            val ex = getExtension(path).lowercase()
-            if (FileSelector.IMAGE_TYPES.contains(ex)) {
-                //图片格式
-                val i = Intent(this, PictureActivity::class.java)
-                i.putExtra("path",path)
-                startActivity(i)
-            } else {
-                openFile(path)
+        mFileAdapter.setListener(object :FileAdapter.BrowserItemListener{
+            override fun onItemClick(path: String) {
+                val ex = getExtension(path).lowercase()
+                if (FileSelector.IMAGE_TYPES.contains(ex)) {
+                    //图片格式
+                    val i = Intent(mContext, PictureActivity::class.java)
+                    i.putExtra("path",path)
+                    startActivity(i)
+                } else {
+                    openFile(path)
+                }
             }
-        }
+
+            override fun onItemLongClick(path: String) {
+                TODO("弹出操作对话框：提取、删除、复制路径、查看详情、重命名")
+            }
+        })
     }
 
     override fun openSearchUI(clazz: KClass<out FileSearchActivity>) {
--- app/src/main/java/xyz/junerver/fsbrowser/OpenSearchActivity.kt
+++ app/src/main/java/xyz/junerver/fsbrowser/OpenSearchActivity.kt
@@ -13,16 +13,22 @@ import xyz.junerver.fileselector.*
 class OpenSearchActivity : FileSearchActivity() {
     override fun initAdapter() {
         super.initAdapter()
-        mFileAdapter.setListener { path ->
-          val ex = getExtension(path).lowercase()
-            if (FileSelector.IMAGE_TYPES.contains(ex)) {
-                //图片格式
-                val i = Intent(this, PictureActivity::class.java)
-                i.putExtra("path",path)
-                startActivity(i)
-            } else {
-                openFile(path)
+        mFileAdapter.setListener(object :FileAdapter.BrowserItemListener{
+            override fun onItemClick(path: String) {
+                val ex = getExtension(path).lowercase()
+                if (FileSelector.IMAGE_TYPES.contains(ex)) {
+                    //图片格式
+                    val i = Intent(mContext, PictureActivity::class.java)
+                    i.putExtra("path",path)
+                    startActivity(i)
+                } else {
+                    openFile(path)
+                }
             }
-        }
+
+            override fun onItemLongClick(path: String) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
